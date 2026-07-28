e779a1b9c01d31fb5d566527acf7c80bc2b76c1f
Riccardo Carlesso <ricc+chezmoi@google.com>
2026-07-28 10:57:28 +0200

    🌙 Fix dark mode: white text on cards + migrate ALL Joomla images (thumbs only, no full-size albums)
    - Dark mode cards now have white text
    - Colored left border accent per card in dark mode
    - Migrated: stories/, riccardo/, tux/, email/, pallit/, zoom/thumbs/
    - Removed viewsize (267MB→72MB)
    - Smileys + emoticons copied for TinyMCE references

---
+++ static/carlesso/img/indexold.php
@@ -0,0 +1,42 @@
+<?
+	$titolo = "Riccardo '486' Carlesso Homepage 2003";
+	//$background= "img/paldocciachiaro.jpg";
+	require_once("header.php");
+?>
+<p align="center"> 
+	<center><h1>The ultimate
+  <br><u>Riccardo </u>&quot;zio 486 Palladius&quot; <u>Carlesso</u>
+  <br>Homepage</h1> 
+</p>
+
+
+
+  
+<div align="center">
+<table width="99%" border="0" cellspacing="0" cellpadding="0">
+    <tr> 
+      <td width="62%" valign="top">
+<div align="center"><a href="indice.php"> <img src="img/carlesso.jpg" width="124" height="160" nosave border="0"> 
+          <img src="img/2003/palladius_frac.jpg"  width="78" height="160" border="0"> 
+          <img src="img/2003/palladius_sbura.jpg" width="113" height="160" border="0"></A></div></td>
+      <td width="38%" valign="top">
+<p align="center"><i><big>Cliccando qui a fianco su una foto a caso avrai un indice 
+          ragionato del sito, per spulciare al meglio ci&ograve; che t'interessa. 
+          Se sei qui il motivo tu lo sai, ma io no. Posso per&ograve; indirizzarti 
+          nelle mie zone preferite...</big></i></p>
+        </td>
+    </tr>
+    <tr> 
+      <td><div align="center"><a href="http://www.goliardia.it" border="0" ><img src="img/logoiside-10k.jpg" border="0"></a></div></td>
+      <td valign="top"><div align="center"><em><big>Un portale costruito completamente 
+          da me che unisce tanti goliardi di tutta Italia (e non solo). Vi piaccia 
+          o no, &egrave;sicuramente una chicca di programmazione (fortuna che 
+          me lo dico da solo...).</big></em></div></td>
+    </tr>
+  </table>
+</div>
+<div align="center"><br/>
+  <?
+	require_once("footer.php");
+?>
+</div>
